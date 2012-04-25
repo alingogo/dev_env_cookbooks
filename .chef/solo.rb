474aba8c9d26cfc625169d70831388af698f7320
Zhoulin Yan <alin.tottori@gmail.com>
2012-04-25 23:04:39 +0900

    add vimrc

---
+++ .chef/solo.rb
@@ -0,0 +1,2 @@
+file_cache_path "/tmp/chef-solo"
+cookbook_path "/home/alin/workspace/dev_env_cookbooks"
---
+++ vimrc/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: vimrc
+# Recipe:: default
+#
+# Copyright 2012, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+#include_recipe "git"
+#include_recipe "vim"
+#include_recipe "ctags"
+
+me = node[:main_user]
+
+case node[:platform]
+when "ubuntu"
+  directory "/home/#{me}/eddie-vim" do
+    owner me
+    group me
+    mode "0755"
+    action :create
+  end
+
+  git "/home/#{me}/eddie-vim" do
+    user me
+    group me
+    repository "git://github.com/kaochenlong/eddie-vim.git"
+    reference "master"
+    action :sync
+  end
+
+  bash "make vimrc" do
+    user me
+    cwd "/home/#{me}"
+    code <<-EOH
+    cd eddie-vim
+    ./update.sh
+    cd ..
+    ln -s eddie-vim .vimtest
+    ln -s eddie-vim/vimrc .vimrctest
+    EOH
+  end
+when "centos"
+  #ToDo
+when "mac"
+  #ToDo
+else
+  #ToDo
+end
